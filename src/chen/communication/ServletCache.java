cbb2d6794ff180c4c1bda8ca79fc6f47f66e6c42
chengchen <chengchen2@hengtiansoft.com>
2017-07-12 17:14:32 +0800

    initial

--- src/chen/communication/ServletCache.java
+++ src/chen/communication/ServletCache.java
@@ -1,8 +1,6 @@
 package chen.communication;
 
 import java.io.IOException;
-
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
@@ -11,51 +9,43 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+
 /**
+ * TODO 不知道缓存用来干嘛的
  * Servlet implementation class Servlet1
  */
-// @WebServlet("/Servlet1")
-public class Servlet1 extends HttpServlet {
+@WebServlet("/ServletCache")
+public class ServletCache extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
 	private ServletConfig config;
 
-	public Servlet1() {
+	public ServletCache() {
 		super();
 	}
 
 	public void init(ServletConfig config) throws ServletException {
 		this.config = config;
-		System.out.println("-----init--1--");
+		System.out.println("init 2=------");
 	}
 
 	protected void service(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		doGet(request, response);
+
 	}
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		ServletContext context = config.getServletContext();
+		System.out.println("servlet2------------");
 
-		String data = "giraffe tree";
+		String data = "abcddfwerwesfasfsadf";
 		/**
-		 * ServletConfig对象中维护了ServletContext对象的引用，开发人员在编写servlet时，
-		 * 可以通过ServletConfig.getServletContext方法获得ServletContext对象。
+		 * 设置数据合理的缓存时间值，以避免浏览器频繁向服务器发送请求，提升服务器的性能 这里是将数据的缓存时间设置为1天
 		 */
-		ServletContext context = config.getServletContext();// 获得ServletContext对象
-		context.setAttribute("data", data); // 将data存储到ServletContext对象中
-		System.out.println("servlet1");
+		response.setDateHeader("expires", System.currentTimeMillis() + 24 * 3600 * 1000);
+		response.getOutputStream().write(data.getBytes());
 		response.getWriter().append("Served at: ").append(request.getContextPath());
-
-		response.getWriter().print("<hr/>");
-
-		String contextInitParam = context.getInitParameter("age");
-		response.getWriter().print(contextInitParam);
-		// 用RequestDispatcher实现请求转发
-		RequestDispatcher rd = context.getRequestDispatcher("/Servlet2");// 获取请求转发对象(RequestDispatcher)
-		rd.forward(request, response);// 调用forward方法实现请求转发
-		
-		
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
