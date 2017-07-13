ae64dedd69250358297cd5a18974d45937a64666
chengchen <chengchen2@hengtiansoft.com>
2017-07-13 09:31:09 +0800

    update

--- src/chen/communication/ServletCache.java
+++ src/chen/communication/ServletCache.java
@@ -43,7 +43,7 @@ public class ServletCache extends HttpServlet {
 		/**
 		 * 设置数据合理的缓存时间值，以避免浏览器频繁向服务器发送请求，提升服务器的性能 这里是将数据的缓存时间设置为1天
 		 */
-		response.setDateHeader("expires", System.currentTimeMillis() + 24 * 3600 * 1000);
+		//response.setDateHeader("expires", System.currentTimeMillis() + 10 * 1000);
 		response.getOutputStream().write(data.getBytes());
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
